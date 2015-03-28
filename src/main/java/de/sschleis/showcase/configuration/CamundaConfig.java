ac1d01db908dc97e0fb9a0a2e74345f83d154e84
sebastian <sebatsian.schleis@gmail.com>
2015-03-28 01:06:47 +0100

    Basic Camunda Engine

---
+++ src/main/java/de/sschleis/showcase/configuration/CamundaConfig.java
@@ -0,0 +1,66 @@
+package de.sschleis.showcase.configuration;
+
+import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.spring.ProcessEngineFactoryBean;
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class CamundaConfig {
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SpringProcessEngineConfiguration springProcessEngineConfiguration(){
+        SpringProcessEngineConfiguration result = new SpringProcessEngineConfiguration();
+        result.setProcessEngineName("engine");
+        result.setDataSource(dataSource);
+        result.setTransactionManager(transactionManager);
+        result.setDatabaseSchemaUpdate("true");
+        result.setJobExecutorActivate(false);
+        return result;
+    }
+
+    @Bean
+    public ProcessEngineFactoryBean processEngineFactoryBean(){
+        ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
+        processEngineFactoryBean.setProcessEngineConfiguration(springProcessEngineConfiguration());
+        return processEngineFactoryBean;
+    }
+
+    @Bean
+    public RepositoryService repositoryService() throws Exception {
+        return processEngineFactoryBean().getObject().getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService() throws Exception {
+        return processEngineFactoryBean().getObject().getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService() throws Exception {
+        return processEngineFactoryBean().getObject().getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService() throws Exception {
+        return processEngineFactoryBean().getObject().getHistoryService();
+    }
+
+    @Bean
+    public ManagementService managementService() throws Exception {
+        return processEngineFactoryBean().getObject().getManagementService();
+    }
+}
